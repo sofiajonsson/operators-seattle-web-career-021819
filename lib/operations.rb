8f7a4716a56013ec2ca047d740ab3ef4144bb4cb
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-02 23:22:04 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
 def unsafe?(speed)
-
+if speed < 40 
+  true
+elsif speed > 60
+  true
+else speed == (40...60)
+  false 
 end
-
+end
 
 
 def not_safe?(speed)
-	
+if speed ? > 60 
+  true
+elsif speed < 40
+true
 end
-	
-
-
+end
